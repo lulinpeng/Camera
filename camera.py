db09f67a6bb79e452f0aea04d3b1caace2ac9026
Linpeng Lu <lu_lin_peng@qq.com>
2025-08-10 21:55:24 +0800

    specify camera

--- camera.py
+++ camera.py
@@ -2,20 +2,21 @@ import cv2
 import os
 import argparse
 import logging
+import math
 
 class Camera:
-    def __init__(self, fps:float=None, width:int=None, height:int=None, cmdfile:str=None):
+    def __init__(self, camera_index:int=None, fps:float=None, width:int=None, height:int=None, cmdfile:str=None):
         logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         logging.getLogger().setLevel(logging.INFO)
         self.logger = logging.getLogger()
+        self.camera_index = 0 if camera_index is None else camera_index
         self.cmdfile = 'cmd.txt' if cmdfile is None else cmdfile
         self.fps = 5.0 if fps is None else fps
         self.width = 640 if width is None else width
         self.height = 480 if height is None else height
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(self.camera_index)
         self.logger.info(f'CAP_PROP_FRAME_WIDTH = {self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)}')
         self.logger.info(f'CAP_PROP_FRAME_HEIGHT = {self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}')
-
         self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
@@ -25,6 +26,17 @@ class Camera:
         # check Video Capture is open
         self.logger.info(f"Is camera open?  {self.cap.isOpened()}")
         return
+    
+    def show_all_cameras(self):
+        cameras = []
+        for i in range(10):
+            cap = cv2.VideoCapture(i)
+            if cap.read()[0]:
+                cameras.append(i)
+                cap.release()
+            else:
+                break
+        return cameras
     
     def get_command(self):
         if os.path.isfile(self.cmdfile):
@@ -45,12 +57,13 @@ class Camera:
         outdir = 'collect_pics' if outdir is None else outdir
         batchfile = 'batch.txt' if batchfile is None else batchfile
         os.makedirs(outdir, exist_ok=True)
+        MAX_IMG_ID = 1000000
         img_id = 0
         imgs = []
         batch_id = 0
         while True:
             cmd = self.get_command()
-            if cmd == 'stop':
+            if cmd == 'stop' or cmd == 'end':
                 self.set_command('')
                 break
             elif cmd == 'batch':
@@ -73,8 +86,8 @@ class Camera:
             if success:
                 cv2.imshow("Image", img)
                 interval = round(1000 / self.fps)
-                img_path = os.path.abspath(os.path.join(outdir, f'{str(img_id).zfill(6)}.jpg'))
-                img_id += 1
+                img_path = os.path.abspath(os.path.join(outdir, f'{str(img_id).zfill(math.ceil(math.log10(MAX_IMG_ID)))}.jpg'))
+                img_id = (img_id + 1) % MAX_IMG_ID
                 self.logger.info(f'collect image {img_path}')
                 cv2.imwrite(img_path, img)
                 imgs.append(img_path)
@@ -89,11 +102,19 @@ if __name__ == '__main__':
     subparsers = parser.add_subparsers( dest="command", title="available commands", metavar="command")
     parser_encode = subparsers.add_parser("collect", help="collect images from camera", description="collect images from camera")
     parser_encode.add_argument('--fps', type=float, default=None, help='frames per second')
+    parser_encode.add_argument('--camera', type=int, choices = [0, 1, 2], default = 0,  help='0 for usb camera, 1 for built-in camera, and 2 for iPhone camera')
+    
+    parser_encode = subparsers.add_parser("show", help="show all availiable cameras", description="show all availiable cameras")
     args = parser.parse_args()
     
     if not hasattr(args, "command") or args.command is None:
         parser.print_help()
     if args.command == 'collect':
         print(f'+++++ collect +++++')
-        camera = Camera(fps = args.fps)
+        camera = Camera(fps = args.fps, camera_index = args.camera)
         camera.collect()
+    elif args.command == 'show':
+        print(f'+++++ show +++++')
+        camera = Camera()
+        cameras = camera.show_all_cameras()
+        print(f'All availiable cameras are {cameras}')
